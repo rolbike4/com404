d9542f751a07c6adf39f3ae3ffa2c9ba1bb801fa
rolbike4 <teodor_libita@yahoo.com>
2019-09-30 11:10:57 +0000

    need more knowledge

---
+++ Game/game-test v1.0.py
@@ -0,0 +1,30 @@
+print("""Hello to my quizz game
+
+For every right answer you get to the next question ....
+
+but for every wrong one you lose one health
+
+You start with 3 lives (that look like these ♥) but you can go up to 6 if you answer corectly the questions
+
+if you lose are your lifes you go back to the begininGOOD LUCK
+""")
+
+print("Please enter your name")
+name = input()
+print("Nice to meet you",name)
+
+print("Okay lets begin... , btw after every question you will be able to see your health... have fun!")
+print("health been set to ♥♥♥")
+
+print("Question 1: What is the name of the German airline?")
+
+life = 3
+pq1 = 1
+pq2 = -1
+q1 = input()
+
+
+if q1 == "Lufthansa":print("Correct you gained a life"), print("Life","♥"*viata),
+viata = life + pq1
+else if:print("Incorrect you lost a life")
+
