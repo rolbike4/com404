0e45f4a0e266d1de9174fc26dc063566f8330c32
rolbike4 <teodor_libita@yahoo.com>
2019-10-03 09:07:52 +0000

    Added code to demonstrate the use of a modulus operator..

--- 1-basics/3-decision/3-if-elif-else/bot.py
+++ 1-basics/3-decision/3-if-elif-else/bot.py
@@ -2,9 +2,9 @@ print("Towards which direction should I paint (up, down, left or right)?")
 direction = input()
 if (direction == "up"):
     print("I am paiting upwards")
-if (direction == "down"):
+elif (direction == "down"):
     print("I am paiting downwards")
-if ( direction == "left"):
+elif ( direction == "left"):
     print("I am paiting leftwards")
-if (direction == "right"):
+elif (direction == "right"):
     print("I am paiting rightwards")
---
+++ 1-basics/3-decision/4-modulo-operator/bot.py
@@ -0,0 +1,8 @@
+print("please enter a whole number")
+
+whole_number = int(input())
+
+if (whole_number % 2 == 0):
+    print("The",whole_number,"is even")
+else:
+    print("The",whole_number,"is odd")
