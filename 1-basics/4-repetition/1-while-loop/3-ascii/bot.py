538214270778266c2f6115077f64f71e8c4a13e7
rolbike4 <teodor_libita@yahoo.com>
2019-10-11 09:26:59 +0000

    Added code to demonstrate the use of summing with a while loop.

--- 1-basics/4-repetition/1-while-loop/3-ascii/bot.py
+++ 1-basics/4-repetition/1-while-loop/3-ascii/bot.py
@@ -4,7 +4,7 @@ print("How many bars should i charge?")
 bars_to_charge = int(input())
 #iteration
 bars_charged = 0
-
+#loop
 while (bars_charged < bars_to_charge):
     bars_charged = bars_charged + 1
     print("Charging", "█" * bars_charged)
---
+++ 1-basics/4-repetition/1-while-loop/5-sum-100/bot.py
@@ -0,0 +1,11 @@
+#print message
+print("Calculating the sum of the first 100 numbers ...")
+
+adding = 0
+
+counting_number = 0
+while (counting_number < 100):
+    counting_number = counting_number + 1
+    adding = adding + counting_number
+
+print(adding)
