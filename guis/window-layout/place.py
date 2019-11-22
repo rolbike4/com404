298f8cf30241cf2c01c6fffe44183ea809c90134
rolbike4 <teodor_libita@yahoo.com>
2019-11-22 10:22:20 +0000

    Grid Place Pack

--- guis/window-layout/place.py
+++ guis/window-layout/place.py
@@ -9,7 +9,7 @@ class Gui(Tk):
 
         # set window properties
         self.title("Newsletter")
-        self.configure(bg="#ccc", padx=10, pady=10)
+        self.configure(bg="#ccc", height=200, width=360)
 
         # add components
         self.__add_outer_frame()
@@ -21,44 +21,42 @@ class Gui(Tk):
 
     def __add_outer_frame(self):
         self.outer_frame = Frame()
-        self.outer_frame.grid(row=0, column=0)
-        self.outer_frame.configure( bg="#eee", 
-                                    padx=10, 
-                                    pady=10)
+        self.outer_frame.place(x=10, y=10, relheight = 0.84, relwidth=0.94)
 
     def __add_heading_label(self):
         self.heading_label = Label(self.outer_frame)
-        self.heading_label.grid(row=0, column=0, columnspan=2)
+        self.heading_label.place(x=20, y=10)
         self.heading_label.configure(   bg="#eee",
-                                        font="Arial 18",
+                                        font="Arial 16",
                                         text="RECEIVE OUR NEWSLETTER")
 
     def __add_instruction_label(self):
         self.instruction_label = Label(self.outer_frame)
-        self.instruction_label.grid(row=1, column=0, columnspan=2, sticky=W)
+        self.instruction_label.place(x=10, y=43)
         self.instruction_label.configure(   bg="#eee",
-                                            text="Please enter your email below to receiver our newsletter")
+                                            justify=LEFT,
+                                            pady=20,
+                                            text="Please enter your email below to receiver our newsletter"
+                                        )
 
     def __add_email_label(self):
         self.email_label = Label(self.outer_frame)
-        self.email_label.grid(row=2, column=0, sticky=E)
-        self.email_label.configure( pady=20,
-                                    text="Email:")
+        self.email_label.place(x=10, y=102)
+        self.email_label.configure( text="Email:")
 
     def __add_email_entry(self):
         self.email_entry = Entry(self.outer_frame)
-        self.email_entry.grid(row=2, column=1, sticky=W)
+        self.email_entry.place(x=50, y= 102)
         self.email_entry.configure(width=40)
 
     def __add_subscribe_button(self):
         self.subscribe_button = Button(self.outer_frame)
-        self.subscribe_button.grid(row=3, column=0, columnspan=2, sticky=N+E+S+W)
+        self.subscribe_button.place(x=0, y=140)
         self.subscribe_button.configure(bg="#fcc",
-                                        text="Subscribe")
-
+                                        text="Subscribe", 
+                                        width=47)
 
 # main.py
-
 from gui import Gui
 
 my_gui = Gui()
